9185f8b6f31e8bf34f7a27cc4db06596f0c8cf8c
hoesnial <husni2518@gmail.com>
2025-04-11 09:27:01 +0700

    fix

--- hapus_detail_aturan.php
+++ hapus_detail_aturan.php
@@ -3,7 +3,7 @@
 $idaturan=$_GET['idaturan'];
 $idgejala=$_GET['idgejala'];
 
-$sql = "DELETE FROM detail_basis_aturan WHERE idaturan='$idaturan' AND idgejala='$idegejala'";
+$sql = "DELETE FROM detail_basis_aturan WHERE idaturan='$idaturan' AND idgejala='$idgejala'";
 if ($conn->query($sql) === TRUE) {
     header("Location:?page=aturan");
 }
